cc313404ef1607e76dcd06513f07e030315bb3c6
Michael Fulbright <mike.fulbright@pobox.com>
2021-06-05 17:30:08 -0400

    Setup Question and Choices and work on corse details showing exam

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
+
+class QuestionInline(admin.ModelAdmin):
+    inlines = [ChoiceInline]
 
 
 class LessonInline(admin.StackedInline):
@@ -28,3 +36,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionInline)
+admin.site.register(Choice)
